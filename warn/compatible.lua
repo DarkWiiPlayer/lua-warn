0574b17992a332a60410ccd7b61dfbb01ce1c25f
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2020-05-12 10:48:58 +0200

    Ignore enabled state when running inside ngx

--- warn/compatible.lua
+++ warn/compatible.lua
@@ -7,16 +7,14 @@ return function(...)
 		elseif ... == "@off" then
 			enabled = false
 		end
+  elseif ngx and ngx.log then
+    ngx.log(ngx.WARN, table.concat({...}, ' '))
 	elseif enabled then
-		if ngx and ngx.log then
-			ngx.log(ngx.WARN, table.concat({...}, ' '))
-		else
-			local prefix = WARN_PREFIX or function()
-				local i = debug.getinfo(3)
-				return "Lua warning ("..i.short_src..":"..i.currentline.."):"
-			end
-			io.stderr:write(table.concat({prefix(), ...}, ' '))
-			io.stderr:write("\n")
-		end
+    local prefix = WARN_PREFIX or function()
+      local i = debug.getinfo(3)
+      return "Lua warning ("..i.short_src..":"..i.currentline.."):"
+    end
+    io.stderr:write(table.concat({prefix(), ...}, ' '))
+    io.stderr:write("\n")
 	end
 end
