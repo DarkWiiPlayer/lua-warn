bc2619d26b41f6e5d408b4eeda9a3a75bd196ad8
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2020-05-12 09:50:22 +0200

    Switch from 💩 spaces to 🤩 tabs

--- warn.lua
+++ warn.lua
@@ -2,5 +2,5 @@ local version = _VERSION:gsub("[^%d]", "")
 version = assert(tonumber(version), "Failed to identify Lua version")
 
 if version < 54 then
-  warn = require 'warn.compatible'
+	warn = require 'warn.compatible'
 end
--- warn/compatible.lua
+++ warn/compatible.lua
@@ -1,22 +1,22 @@
 local enabled = false
 
 return function(...)
-  if select('#', ...) == 1 and (...):byte()==64 then
-    if ... == "@on" then
-      enabled = true
-    elseif ... == "@off" then
-      enabled = false
-    end
-  elseif enabled then
-    if ngx and ngx.log then
-      ngx.log(ngx.WARN, table.concat({...}, ' '))
-    else
-      local prefix = WARN_PREFIX or function()
-        local i = debug.getinfo(3)
-        return "Lua warning ("..i.short_src..":"..i.currentline.."):"
-      end
-      io.stderr:write(table.concat({prefix(), ...}, ' '))
-      io.stderr:write("\n")
-    end
-  end
+	if select('#', ...) == 1 and (...):byte()==64 then
+		if ... == "@on" then
+			enabled = true
+		elseif ... == "@off" then
+			enabled = false
+		end
+	elseif enabled then
+		if ngx and ngx.log then
+			ngx.log(ngx.WARN, table.concat({...}, ' '))
+		else
+			local prefix = WARN_PREFIX or function()
+				local i = debug.getinfo(3)
+				return "Lua warning ("..i.short_src..":"..i.currentline.."):"
+			end
+			io.stderr:write(table.concat({prefix(), ...}, ' '))
+			io.stderr:write("\n")
+		end
+	end
 end
