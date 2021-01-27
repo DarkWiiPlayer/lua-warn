2cbbcba1710c9e524e22bdd8e135f7f7cc1a616c
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2021-01-27 15:27:40 +0100

    Make w turn on warn.compatible warnings even on 5.4+

--- w.lua
+++ w.lua
@@ -1,3 +1,5 @@
 --- Allows using lua -lw instead of lua -W for compatibility
 require 'warn'
 warn("@on")
+-- In case this is used explicitly in Lua 5.4+, turn it on separately:
+require 'warn.compatible' '@on'
--- warn/compatible.lua
+++ warn/compatible.lua
@@ -8,10 +8,10 @@ return function(...)
 			enabled = false
 		end
 	elseif enabled then
-    local prefix = WARN_PREFIX or function()
-      local i = debug.getinfo(3)
-      return "Lua warning ("..i.short_src..":"..i.currentline.."):"
-    end
-    io.stderr:write(table.concat({prefix(), ...}, ' '), "\n")
+		local prefix = WARN_PREFIX or function()
+			local i = debug.getinfo(3)
+			return "Lua warning ("..i.short_src..":"..i.currentline.."):"
+		end
+		io.stderr:write(table.concat({prefix(), ...}, ' '), "\n")
 	end
 end
